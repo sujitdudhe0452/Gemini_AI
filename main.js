27b85db10f3501a7513db0849e4d931532b1f37b
sujitdudhe0452 <sujitdudhe.2003@gmail.com>
2024-10-15 00:50:18 +0530

    THIS IS THE FINAL GEMINI_API INTEGRATION

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
-// Make sure to include these imports:
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require('dotenv').config();
+const express = require('express'); 
+const app = express();
+app.use(express.json()); 
+
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
+app.get('/', (req, res) => {
+    res.send("Kya..Developer banega re tuuu..");
+});
 
-const prompt = "Which is the best course start carrer in IT field.?";
+const generate = async (prompt) => {
+    try 
+    {
+      const result = await model.generateContent(prompt);
+      console.log(result.response.text());
+      return result.response.text();   
+    } catch (err) {
+        console.log(err);
+    }
+}
+// generate();
 
-const generate = async()=>
+app.get('/api/content',async(req,res)=>
 {
     try
     {
-        const result = await model.generateContent(prompt);
-        console.log(result.response.text());
+        const data = req.body.questions;
+        const result = await generate(data);
+        res.send({
+            "result":result
+        })
     }
     catch(err)
     {
         console.log(err);
     }
-}
-
-generate();
+})
+app.listen(3000, () => {
+    console.log('Server is up and running on port 3000');
+})
